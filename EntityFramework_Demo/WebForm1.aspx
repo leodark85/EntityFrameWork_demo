288cef21f708e0099c651f4bb113bcf5ee257d86
leodark85 <leodark85@gmail.com>
2018-09-15 19:26:53 -0500

    Entity framework demo
    por alguna razon cuando realizo el edit  despues del postback se muestran el boton update en el row del grid que se acaba de editar y los  botones de edit y delete desaparecen , lo revisare mas tarde

---
+++ EntityFramework_Demo/WebForm1.aspx
@@ -0,0 +1,61 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="WebForm1.aspx.cs" Inherits="EntityFramework_Demo.WebForm1" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <div>
+            <table >
+                <tr>
+                    <td>Nombre:</td>
+                    <td>
+                        <asp:TextBox ID="TextBox1" runat="server"></asp:TextBox>
+                        <asp:Label ID="Label1" runat="server" Text="Label" Visible="False"></asp:Label>
+                    </td>
+                    <td>&nbsp;</td>
+                </tr>
+                <tr>
+                    <td>email:</td>
+                    <td>
+                        <asp:TextBox ID="TextBox2" runat="server"></asp:TextBox>
+                    </td>
+                    <td>&nbsp;</td>
+                </tr>
+                <tr>
+                    <td>Birth Date:</td>
+                    <td>
+                        <asp:TextBox ID="TextBox3" runat="server"></asp:TextBox>
+                    </td>
+                    <td>&nbsp;</td>
+                </tr>
+                <tr>
+                    <td>
+                        
+                    </td>
+                    <td>
+
+                        <asp:Button ID="Button1" runat="server" Text="Save Data" OnClick="Button1_Click" />
+
+                    </td>
+                    <td></td>
+                </tr>
+            </table>
+        </div>
+        <div>
+            <asp:GridView ID="GridView1" runat="server" OnRowDeleting="GridView1_RowDeleting" OnRowEditing="GridView1_RowEditing">
+                <Columns>
+                    <asp:CommandField ButtonType="Button" ShowCancelButton="False" ShowDeleteButton="True" ShowEditButton="True" />
+                </Columns>
+            </asp:GridView>
+        </div>
+    </form>
+    <div>
+    </div>
+    <div>
+    </div>
+</body>
+</html>
---
+++ EntityFramework_Demo/WebForm1.aspx.cs
@@ -0,0 +1,102 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace EntityFramework_Demo
+{
+    public partial class WebForm1 : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if (!IsPostBack)
+            {
+                Label1.Text = string.Empty;
+            }
+            GridViewReloadData();
+        }
+
+        private void GridViewReloadData()
+        {
+            using (CompanyEntities_2 ent = new CompanyEntities_2())
+            {
+                GridView1.DataSource = ent.Users.ToList();
+                GridView1.DataBind();
+               
+            }
+        }
+        private void CleanControls ()
+        {
+            TextBox1.Text = string.Empty;
+            TextBox2.Text = string.Empty;
+            TextBox3.Text = string.Empty;
+        }
+        protected void Button1_Click(object sender, EventArgs e)
+        {
+            using (CompanyEntities_2 Ent = new CompanyEntities_2())
+            {
+                if (Label1.Text != string.Empty)
+                {
+                    int Id = int.Parse(Label1.Text);
+                    var UserToEdit = Ent.Users.Where(R => R.IdUsuario == Id).First();
+                    UserToEdit.Name = TextBox1.Text;
+                    UserToEdit.email = TextBox2.Text;
+                    UserToEdit.birthDate = TextBox3.Text;
+                    Ent.SaveChanges();
+                    CleanControls();
+                 
+
+                }
+                else
+                {
+                    User Newuser = new User();
+                    Newuser.Name = TextBox1.Text;
+                    Newuser.email = TextBox2.Text;
+                    Newuser.birthDate = TextBox3.Text;
+                    Ent.Users.Add(Newuser);
+                    Ent.SaveChanges();
+                }
+            }
+            GridViewReloadData();
+        }
+
+
+        protected void GridView1_RowEditing(object sender, GridViewEditEventArgs e)
+        {
+           
+            int idUser = int.Parse(GridView1.Rows[e.NewEditIndex].Cells[1].Text);
+            using (CompanyEntities_2 ent = new CompanyEntities_2())
+            {
+                var UserToEdit= ent.Users.Where(R => R.IdUsuario == idUser).First(); ;
+                Label1.Text = UserToEdit.IdUsuario.ToString();
+                TextBox1.Text = UserToEdit.Name;
+                TextBox2.Text = UserToEdit.email;
+                TextBox3.Text = UserToEdit.birthDate;
+                ent.SaveChanges();
+            }
+            GridViewReloadData();
+        }
+
+        protected void GridView1_RowDeleting(object sender, GridViewDeleteEventArgs e)
+        {
+            
+            int  idUser =int.Parse( GridView1.Rows[e.RowIndex].Cells[1].Text);
+            using (CompanyEntities_2 ent = new CompanyEntities_2())
+            {
+                var  UserToDelete = ent.Users.Where(R => R.IdUsuario == idUser).First(); ;
+                ent.Users.Remove(UserToDelete);
+                ent.SaveChanges(); 
+            }
+            GridViewReloadData();
+        }
+
+        protected void Button2_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+
+       
+    }
